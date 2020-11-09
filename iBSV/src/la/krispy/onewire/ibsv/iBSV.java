a9cce0241943056e1a0591ab8c8fcf9a696030a8
Krispy <42679658+kr1spyK@users.noreply.github.com>
2020-11-09 17:07:08 -0500

    Formatting

--- iBSV/src/la/krispy/onewire/ibsv/iBSV.java
+++ iBSV/src/la/krispy/onewire/ibsv/iBSV.java
@@ -50,95 +50,95 @@ public class iBSV {
         DSPortAdapter adapter;
         boolean adapterdetected = false;
         
-        while (!adapterdetected) {
-            try {
-                // get the default adapter
-                adapter = OneWireAccessProvider.getDefaultAdapter();
-                adapterdetected = adapter.adapterDetected();
+    while (!adapterdetected) {
+        try {
+            // get the default adapter
+            adapter = OneWireAccessProvider.getDefaultAdapter();
+            adapterdetected = adapter.adapterDetected();
 
-                System.out.println();
-                System.out.println("SecureViewer for DS1991 iButton - Java console app");
-                System.out.println("AdapterVersion: " + adapter.getAdapterVersion()
-                                + "; Port: " + adapter.getPortName());
-                
-                System.out.println("canDeliverPower: "
-                                + adapter.canDeliverPower()
-                                + ", smart power: "
-                                + adapter.canDeliverSmartPower());
+            System.out.println();
+            System.out.println("SecureViewer for DS1991 iButton - Java console app");
+            System.out.println("AdapterVersion: " + adapter.getAdapterVersion()
+                            + "; Port: " + adapter.getPortName());
+            
+            System.out.println("canDeliverPower: "
+                            + adapter.canDeliverPower()
+                            + ", smart power: "
+                            + adapter.canDeliverSmartPower());
 
-                /**
-                 * SESSION: Negotiate exclusive use of 1-Wire bus
-                 */
-                adapter.beginExclusive(true);
-                
-                /**
-                 * LINK: connect to 1-Wire bus 
-                 */
-                // clear previous search restrictions
-                adapter.setSearchAllDevices();
-                adapter.targetAllFamilies();
-                adapter.setSpeed(DSPortAdapter.SPEED_REGULAR);
+            /**
+             * SESSION: Negotiate exclusive use of 1-Wire bus
+             */
+            adapter.beginExclusive(true);
+            
+            /**
+             * LINK: connect to 1-Wire bus 
+             */
+            // clear previous search restrictions
+            adapter.setSearchAllDevices();
+            adapter.targetAllFamilies();
+            adapter.setSpeed(DSPortAdapter.SPEED_REGULAR);
 
-                /**
-                 * NETWORK: Device discovery and selection
-                 */
-                System.out.println();
-                System.out.println("Detected 1-Wire devices:");
-                for (@SuppressWarnings("unchecked")
-                    Enumeration<OneWireContainer> owd_enum = adapter.getAllDeviceContainers();
-                            owd_enum.hasMoreElements(); ) {
-                    owd = owd_enum.nextElement();
+            /**
+             * NETWORK: Device discovery and selection
+             */
+            System.out.println();
+            System.out.println("Detected 1-Wire devices:");
+            for (@SuppressWarnings("unchecked")
+                Enumeration<OneWireContainer> owd_enum = adapter.getAllDeviceContainers();
+                        owd_enum.hasMoreElements(); ) {
+                owd = owd_enum.nextElement();
 
-                    System.out.printf("%s%n", owd.toString());
-                }
-
-                //     DS1991 MultiKey, family code 02 (hex).
-                adapter.targetFamily(Convert.toInt("02"));
-                // get first DS1991 that we see, for now.
-                owd = adapter.getFirstDeviceContainer();
-                Long owdAddress = owd.getAddressAsLong();
-                OneWireContainer02 onewirecontainer02 = new OneWireContainer02(adapter, owd.getAddressAsLong()); 
-
-                System.out.printf("=== %s ===%n%s%n", owd.getName(), owd.getDescription());
-                System.out.printf("=== %n working with: %s%n", owd.getAddressAsString());
-
-                byte scratchpad[] = new byte[64];
-
-                // TODO: Create clear scratchpad method
-                System.out.println("Clearing scratchpad...");
-                Arrays.fill(scratchpad, (byte) 0x55);
-                onewirecontainer02.writeScratchpad(00, scratchpad);
-
-                System.out.println("Read Scratchpad:");
-                System.out.println(Convert.toHexString(onewirecontainer02.readScratchpad(), " "));
-                System.out.println(onewirecontainer02.readScratchpad());
-
-                /**
-                 * TRANSPORT: DS1991 only supports primative memory functions and some
-                 * unique commands
-                 */
-                // Writing scratchpad takes int starting address (0x00 to 0x3F) & byte[] data.
-                String strData = "The quest for hot dogs is on";
-               /* byte[] */ scratchpad = strData.getBytes();
-                onewirecontainer02.writeScratchpad(Convert.toInt("00"), scratchpad);
-                System.out.println("writing message to scratchpad...");
-                System.out.println(Convert.toHexString(onewirecontainer02.readScratchpad(), " "));
-
-                // unblock adapter and free the port
-                adapter.endExclusive();
-                adapter.freePort();
-                
-                //Testing program running 
-                break;
+                System.out.printf("%s%n", owd.toString());
             }
-            catch (OneWireIOException e) {
-                System.out.println(e + " Adapter communication failure");
-                continue;                
-            }
-            catch (OneWireException e) {
-                System.out.println(e + " Adapter/Port not available.");
-                return;
-            }
+
+            //     DS1991 MultiKey, family code 02 (hex).
+            adapter.targetFamily(Convert.toInt("02"));
+            // get first DS1991 that we see, for now.
+            owd = adapter.getFirstDeviceContainer();
+            Long owdAddress = owd.getAddressAsLong();
+            OneWireContainer02 onewirecontainer02 = new OneWireContainer02(adapter, owd.getAddressAsLong()); 
+
+            System.out.printf("=== %s ===%n%s%n", owd.getName(), owd.getDescription());
+            System.out.printf("=== %n working with: %s%n", owd.getAddressAsString());
+
+            byte scratchpad[] = new byte[64];
+
+            // TODO: Create clear scratchpad method
+            System.out.println("Clearing scratchpad...");
+            Arrays.fill(scratchpad, (byte) 0x55);
+            onewirecontainer02.writeScratchpad(00, scratchpad);
+
+            System.out.println("Read Scratchpad:");
+            System.out.println(Convert.toHexString(onewirecontainer02.readScratchpad(), " "));
+            System.out.println(onewirecontainer02.readScratchpad());
+
+            /**
+             * TRANSPORT: DS1991 only supports primative memory functions and some
+             * unique commands
+             */
+            // Writing scratchpad takes int starting address (0x00 to 0x3F) & byte[] data.
+            String strData = "The quest for hot dogs is on";
+            /* byte[] */ scratchpad = strData.getBytes();
+            onewirecontainer02.writeScratchpad(Convert.toInt("00"), scratchpad);
+            System.out.println("writing message to scratchpad...");
+            System.out.println(Convert.toHexString(onewirecontainer02.readScratchpad(), " "));
+
+            // unblock adapter and free the port
+            adapter.endExclusive();
+            adapter.freePort();
+            
+            //Testing program running 
+            break;
         }
+        catch (OneWireIOException e) {
+            System.out.println(e + " Adapter communication failure");
+            continue;                
+        }
+        catch (OneWireException e) {
+            System.out.println(e + " can't find Adapter/Port.");
+            return;
+        }
+    }
     }
 }
