a99a29bde0ccd8b9630ddb7129a8fabe284dcc26
Krispy <42679658+kr1spyK@users.noreply.github.com>
2020-11-09 15:53:54 -0500

    Added clearing scratchpad, r/w testing

--- iBSV/src/la/krispy/onewire/ibsv/iBSV.java
+++ iBSV/src/la/krispy/onewire/ibsv/iBSV.java
@@ -33,8 +33,10 @@ import com.dalsemi.onewire.OneWireException;
 import com.dalsemi.onewire.adapter.DSPortAdapter;
 import com.dalsemi.onewire.adapter.OneWireIOException;
 import com.dalsemi.onewire.container.OneWireContainer;
+import com.dalsemi.onewire.container.OneWireContainer02;
 import com.dalsemi.onewire.utils.Convert;
 
+import java.util.Arrays;
 import java.util.Enumeration;
 
 /**
@@ -87,19 +89,39 @@ public class iBSV {
                             owd_enum.hasMoreElements(); ) {
                     owd = owd_enum.nextElement();
 
-                    String owdAddress = owd.getAddressAsString();
-                    System.out.printf("%s {%s} %n", owdAddress, owd.getName());
+                    System.out.printf("%s%n", owd.toString());
                 }
 
                 //     DS1991 MultiKey, family code 02 (hex).
                 adapter.targetFamily(Convert.toInt("02"));
+                // get first DS1991 that we see, for now.
                 owd = adapter.getFirstDeviceContainer();
+                Long owdAddress = owd.getAddressAsLong();
+                OneWireContainer02 onewirecontainer02 = new OneWireContainer02(adapter, owd.getAddressAsLong()); 
+
+                System.out.printf("=== %s ===%n%s%n", owd.getName(), owd.getDescription());
                 System.out.printf("working with: %s%n", owd.getAddressAsString());
+
+                byte scratchpad[] = new byte[64];
+
+                // TODO: Create clear scratchpad method
+                System.out.println("Clearing scratchpad...");
+                Arrays.fill(scratchpad, (byte) 0x55);
+                onewirecontainer02.writeScratchpad(00, scratchpad);
+
+                System.out.println("Read Scratchpad:");
+                System.out.println(Convert.toHexString(onewirecontainer02.readScratchpad(), " "));
 
                 /**
                  * TRANSPORT: DS1991 only supports primative memory functions and some
                  * unique commands
                  */
+                // Writing scratchpad takes int starting address (0x00 to 0x3F) & byte[] data.
+                String strData = "The quest for hot dogs is on";
+               /* byte[] */ scratchpad = strData.getBytes();
+                onewirecontainer02.writeScratchpad(Convert.toInt("00"), scratchpad);
+                System.out.println("writing message to scratchpad...");
+                System.out.println(Convert.toHexString(onewirecontainer02.readScratchpad(), " "));
 
                 // unblock adapter and free the port
                 adapter.endExclusive();
